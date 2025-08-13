9b4858af0d9033fa23f02f98d86fbe732595c285
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-08-13 07:14:47 +0000

    backend-test: add learning serializers tests

---
+++ investa_backend/test/test_learning_serializers_309.py
@@ -0,0 +1,20 @@
+"""
+Tests for learning serializers
+"""
+
+from django.test import TestCase
+from rest_framework.test import APIClient
+
+
+class Testlearningserializers(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_list(self):
+        response = self.client.get("/api/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_create(self):
+        data = {}
+        response = self.client.post("/api/", data)
+        self.assertIn(response.status_code, [200, 201, 400])
