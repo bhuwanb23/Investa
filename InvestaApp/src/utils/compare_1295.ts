dc5015c5775e376444b297433c047bebb2a4146f
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-11-19 07:14:47 +0000

    feat: add compare tests

---
+++ InvestaApp/src/utils/compare_1295.ts
@@ -0,0 +1,7 @@
+/**
+ * compare utility functions
+ */
+
+export const formatDate = (d: Date) => d.toLocaleDateString();
+export const formatTime = (d: Date) => d.toLocaleTimeString();
+export const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
